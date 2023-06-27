aae31ff8a4fa499160e8433b75b55d4f848dfd00
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-06-27 16:51:46 +0100

    lomuto quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,70 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sort an array of int with quick sort
+ * @array: array to sort
+ * @size: size of the array
+*/
+
+void quick_sort(int *array, size_t size)
+{
+	partition(array, 0, size - 1, size);
+}
+
+/**
+ * partition - partitioning an array
+ * @array: array to sort
+ * @low: lowest index
+ * @high: highest index
+ * @size: size of the array
+*/
+
+void partition(int *array, int low, int high, size_t size)
+{
+	int pivot;
+
+	if (low > high)
+		return;
+	pivot = lomuto(array, low, high, size);
+	partition(array, low, pivot - 1, size);
+	partition(array, pivot + 1, high, size);
+}
+
+/**
+ * lomuto - lomuto partition
+ * @array: array to sort
+ * @low: low
+ *@high: high
+ * @size: size
+ * Return: index of the pivot after swaping
+*/
+
+int lomuto(int *array, int low, int high, size_t size)
+{
+	int tmp;
+	int pivot = array[high];
+	int i, j;
+
+	for (i = j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			if (j != i)
+			{
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+	if (i != high)
+	{
+		tmp = array[i];
+		array[i] = array[high];
+		array[high] = tmp;
+		print_array(array, size);
+	}
+	return (i);
+}
